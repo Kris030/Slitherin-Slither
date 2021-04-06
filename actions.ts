f72e277b86636af4bdd155f9f37a9bee55849c0c
the boi <kristof.bacskai03@gmail.com>
2021-04-06 13:10:42 +0200

    Add test commands

--- actions.ts
+++ actions.ts
@@ -1,4 +1,5 @@
 import { Message, User } from 'discord.js';
+import { textDialog } from './Utils/dialog.js';
 import { emojifyString } from './Utils/emojis.js';
 import MessageAction from './MessageActions/MessageAction.js';
 import { PrefixCommand, TypedPrefixCommand } from './Utils/actions.js';
@@ -10,7 +11,35 @@ const as = [
 			this.msg.channel.send(emojifyString(data[0]), {
 				split: true
 			});
-		})
+		}),
+
+	new MessageAction()
+		.condition(msg => msg.content.includes('daddy'))
+		.action(msg => msg.react('🥵')),
+
+	PrefixCommand('sstalk')
+		.action(async function(data) {
+				const path = await textDialog(this.msg.channel, this.msg.author, {
+					prompt: 'prompt #0',
+					responses: [
+						{
+							answer: 'answer 1',
+							branch: {
+								prompt: 'prompt #0 #0'
+							}
+						}, {
+							answer: 'answer 2',
+							branch: {
+								prompt: 'prompt #0 #1'
+							}
+						},
+					]
+				}).path;
+
+				this.msg.channel.send('Path: ' + arrayToString(path));
+			}),
+
+
 ] as any as MessageAction<Message>[];
 
 export default as;
