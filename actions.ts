4d946fdb4f6eced8f84698aa33a50bdd371cdff3
the boi <kristof.bacskai03@gmail.com>
2021-04-06 16:34:59 +0200

    Change actions to be per message

--- actions.ts
+++ actions.ts
@@ -5,11 +5,12 @@ import MessageAction from './MessageActions/MessageAction.js';
 import { PrefixCommand, TypedPrefixCommand } from './Utils/actions.js';
 import { arrayToString, sleep, getRandomElement } from './Utils/general.js';
 
-const as = [
+export const createActions = () => [
 	PrefixCommand('ssemojify', { parseFully: false })
-		.action(function(data) {
-			this.msg.channel.send(emojifyString(data[0]), {
-				split: true
+		.action(async function(data) {
+			await this.msg.channel.send(emojifyString(data[0]), {
+				split: true,
+				code: ' '
 			});
 		}),
 
@@ -18,28 +19,26 @@ const as = [
 		.action(msg => msg.react('🥵')),
 
 	PrefixCommand('sstalk')
-		.action(async function(data) {
-				const path = await textDialog(this.msg.channel, this.msg.author, {
-					prompt: 'prompt #0',
-					responses: [
-						{
-							answer: 'answer 1',
-							branch: {
-								prompt: 'prompt #0 #0'
-							}
-						}, {
-							answer: 'answer 2',
-							branch: {
-								prompt: 'prompt #0 #1'
-							}
-						},
-					]
-				}).path;
-
-				this.msg.channel.send('Path: ' + arrayToString(path));
-			}),
-
+		.action(async function() {
+			const path = await textDialog(this.msg.channel, this.msg.author, {
+				prompt: 'prompt #0',
+				responses: [
+					{
+						answer: 'answer 1',
+						branch: {
+							prompt: 'prompt #0 #0'
+						}
+					}, {
+						answer: 'answer 2',
+						branch: {
+							prompt: 'prompt #0 #1'
+						}
+					},
+				]
+			}).path;
+			
+			await this.msg.channel.send('Path: ' + arrayToString(path));
+		})
+		.onError((data, err) => console.error(err)),
 
 ] as any as MessageAction<Message>[];
-
-export default as;
