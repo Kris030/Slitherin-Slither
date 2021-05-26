3c59cacb323064c86623d018f2de69ccfcb55c4b
Kristóf Bácskai <kristof.bacskai03@gmail.com>
2021-05-26 20:26:46 +0200

    Hotfix ssrand (swapped min max)

--- res/MessageActions/misc.ts
+++ res/MessageActions/misc.ts
@@ -3,7 +3,7 @@ import { TypedPrefixCommand } from '../../src/Utils/actions.js';
 export default [
 	TypedPrefixCommand('ssrand', {}, Number, Number, Boolean)
 		.action(function(args) {
-			let n = Math.random() * args[0] + args[1];
+			let n = Math.random() * args[1] + args[0];
 			if (!args[2])
 				n = Math.round(n);
 
